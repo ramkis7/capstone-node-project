41c93e9cd08bdd0e8cee9a5831445d8297765f59
ramkis7 <169043618+ramkis7@users.noreply.github.com>
2025-10-08 17:05:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 const http = require('http');
-
 const PORT = 3000;
 
-
-
 const demoCard = `
-
 <section class="card">
-
     <h2>Ramki Captone Project </h2>
-
     <p>Jenkins Pipeline</p><p>Quality Gate</p><p>Success Card</p>
-
 </section>`;
 
-
-
 const server = http.createServer((req, res) => {
-
     res.writeHead(200, {'Content-Type':'text/html'});
-
     res.end(`<h1>Hello from Node.js on Capstone Project!</h1>
-
 ${demoCard}
-
 `);
-
 });
-
-
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
