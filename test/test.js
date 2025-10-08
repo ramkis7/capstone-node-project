423f61c199d00d54a56c29627d22e19daa447145
ramkis7 <169043618+ramkis7@users.noreply.github.com>
2025-10-08 16:36:35 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,30 @@
-
 const assert = require('assert');
 const http = require('http');
+const path = require('path');
+const { spawn } = require('child_process');
+
+const serverFile = path.join(__dirname, '..', 'index.js');
+let serverProcess;
 
 describe('Node.js App', function() {
+  this.timeout(5000); // Set a timeout for tests
+
+  before(function(done) {
+    // Start the Node.js server before running tests
+    serverProcess = spawn('node', [serverFile]);
+    serverProcess.stdout.on('data', (data) => {
+      // Wait for the server to log that it's running
+      if (data.toString().includes('Server running')) {
+        done();
+      }
+    });
+  });
+
+  after(function() {
+    // Stop the server after tests are done
+    serverProcess.kill('SIGINT');
+  });
+
   it('should return 200 OK for a GET request to /', function(done) {
     http.get('http://localhost:3000', (res) => {
       assert.strictEqual(res.statusCode, 200);
